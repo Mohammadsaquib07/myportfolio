60f5693a8fe3241caad9e4210629bfa531d2fe31
mohammad_saquib <saquibkudle08@gmail.com>
2026-01-05 17:46:48 +0530

    UI updated

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+ socialLinks = [
+    { icon: 'bi-geo-alt-fill', text: 'Solapur, Inida', link: null },
+    { icon: 'bi-envelope-fill', text: 'saquib@gmail.com', link: 'mailto:saquibkudle08@gmail.com' },
+    { icon: 'bi-telephone-fill', text: '+918080727590', link: 'tel:+918080727590' },
+    { icon: 'bi-linkedin', text: 'LinkedIn', link: 'www.linkedin.com/in/mohammad-saquib-kudle-191351227' },
+    { icon: 'bi-github', text: 'GitHub', link: 'https://github.com/Mohammadsaquib07' },
+  ];
 }
