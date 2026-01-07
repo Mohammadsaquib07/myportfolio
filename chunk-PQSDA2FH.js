29696696743081946076e97c0687875031883491
mohammad_saquib <saquibkudle08@gmail.com>
2026-01-07 13:05:20 +0530

    Auto-generated commit

---
+++ chunk-PQSDA2FH.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-52YAE62Q.js";import"./chunk-F4U7ZLKM.js";import"./chunk-F7DQGDWK.js";export{a as AboutComponent};
---
+++ chunk-XS577MEI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-AFISVP23.js";import"./chunk-F7DQGDWK.js";export{a as SkillsComponent};
